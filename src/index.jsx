f1deab0e6dc8eac3b0ffa7c147710192730b3696
Joyce Kao <joyceykao@gmail.com>
2020-10-01 14:45:30 +0200

    added app into index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app.jsx';
 
 const Hello = ({ name }) => {
   return (
